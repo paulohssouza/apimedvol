f9ab03992e07ac64f14458630fa6c7a1e62a6420
phbarbudo <paulohss.souza@gmail.com>
2023-08-05 20:33:31 -0300

    Start of the api project using the spring boot framework for management simulation of a medical clinic

---
+++ src/main/java/br/com/med/vol/apimedvol/ApimedvolApplication.java
@@ -0,0 +1,13 @@
+package br.com.med.vol.apimedvol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimedvolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApimedvolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/med/vol/apimedvol/ApimedvolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.med.vol.apimedvol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimedvolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
