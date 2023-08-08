23562b6bbde05de0ad5a191c3586470783116d23
phbarbudo <paulohss.souza@gmail.com>
2023-08-08 11:12:17 -0300

    Creation entity user and migration for create table in database

---
+++ src/main/java/br/com/med/vol/apimedvol/model/user/User.java
@@ -0,0 +1,21 @@
+package br.com.med.vol.apimedvol.model.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "User")
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String password;
+}
